5d9b2f60ae31c125f04fe801daf792612a9a96cc
gapus97 <gaspersmerkolj@gmail.com>
2020-05-11 17:09:35 +0200

    Add GlobalProperties, ApplicationConstants and configure build gradle and application.yml

--- src/main/java/fun7App/SpringBootFun7Application.java
+++ src/main/java/fun7App/SpringBootFun7Application.java
@@ -3,12 +3,12 @@ package fun7App;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @EnableJpaRepositories(basePackages = "fun7App.repository")
 @SpringBootApplication
-public class Application {
+public class SpringBootFun7Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(SpringBootFun7Application.class, args);
     }
 }
+
---
+++ src/main/java/fun7App/utilities/ApplicationConstants.java
@@ -0,0 +1,14 @@
+package fun7App.utilities;
+
+public class ApplicationConstants {
+    public static int START_OF_CUSTOMER_SERVICE = 9;
+    public static int END_OF_CUSTOMER_SERVICE = 15;
+    public static int MULTIPLAYER_WHEN_MORE_THAN = 5;
+    public static String ADS_ENABLED_SUBSTRING = "sure, why not!";
+    public static String BAD_REQUEST_MESSAGE = "missing mandatory parameters";
+    public static String UNAUTHORIZED_MESSAGE = "invalid credentials";
+    public static String INTERNAL_SERVER_ERROR_MESSAGE = "server is temporarily not available";
+    public static String SERVICE_ENABLED = "enabled";
+    public static String SERVICE_DISABLED = "disabled";
+
+}
